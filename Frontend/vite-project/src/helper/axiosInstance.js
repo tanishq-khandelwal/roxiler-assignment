a020c3f9f8755bdd86beb89ee2039f02e1c5e021
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2024-10-09 08:53:47 +0530

    pushed to prod

--- Frontend/vite-project/src/helper/axiosInstance.js
+++ Frontend/vite-project/src/helper/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const LOCAL_URL="http://localhost:3000/api/v1";
-const PROD_URL="https://fullstack-medvault.onrender.com/api/v1"
+const PROD_URL="https://roxiler-assignment-lz2q.onrender.com/api/v1"
 const axiosInstance=axios.create();
 
-axiosInstance.defaults.baseURL=LOCAL_URL;
+axiosInstance.defaults.baseURL=PROD_URL;
 axiosInstance.defaults.withCredentials=true;
 
 
